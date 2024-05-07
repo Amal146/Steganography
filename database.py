6f6bc98191eac486898458d2b2e40c60f8d30905
amal <amaljw2002@gmail.com>
2024-05-07 20:44:33 +0100

    new file:   database.py 	modified:   encoded_1.png 	modified:   main.py 	new file:   models.py 	modified:   requirements.txt

---
+++ database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+database_url = "postgresql://postegres:system@localhost/pixhide"
+engine = create_engine(database_url)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
