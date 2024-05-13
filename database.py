ea79a056fcf1d1eadc2366d03cc768ad080a630a
Amal146 <140029631+Amal146@users.noreply.github.com>
2024-05-13 19:32:53 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-database_url = "postgresql://postgres:system@localhost/pixhide"
+database_url = "postgresql://postgres:system@localhost:5432/pixhide"
 engine = create_engine(database_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
